c19ec770ba6b2768cc988bc35d4e30c220e34d71
Swag-blips <coderblip@gmail.com>
2024-12-12 22:03:07 +0100

    feat:type definitions, mutations and resolvers

---
+++ server/server.js
@@ -0,0 +1,51 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const users = [
+  { id: "1", name: "John Doe", age: 30, isMarried: true },
+  { id: "2", name: "Jane Smith", age: 25, isMarried: false },
+  { id: "3", name: "Alice Johnson", age: 40, isMarried: true },
+  { id: "4", name: "Bob Brown", age: 35, isMarried: false },
+  { id: "5", name: "Charlie Davis", age: 28, isMarried: true },
+];
+
+const typeDefs = `
+type Query{
+getUsers: [User]
+getUserById(id:ID): User
+}
+
+type Mutation{
+createUser(name: String!, age:Int!, isMarried: Boolean!): User
+}
+
+type User{
+ id: ID
+ name: String
+ age:Int
+ isMarried: Boolean
+}
+`;
+
+const resolvers = {
+  Query: {
+    getUsers: () => {
+      return users;
+    },
+    getUserById: (parent, args) => {
+      const id = args.id;
+      users.find((user) => user.id === id);
+    },
+  },
+  Mutation: {},
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`server running on ${url}`);
+
+// Query mutation
+// typeDefs, resolvers
